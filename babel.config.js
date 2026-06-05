8da34e90844d044ded197ce09baae0ed7a07fa6e
Ace51205 <midnightcompiling@gmail.com>
2026-06-05 09:16:46 -0700

    all remove

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [
-    'module:metro-react-native-babel-preset',
-    '@babel/preset-typescript',
-  ],
-  plugins: [['@babel/plugin-transform-private-methods', { loose: true }]],
-}
--- jest.native.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'react-native',
-  transformIgnorePatterns: [
-    'node_modules/.pnpm/(?!react-native-payfort-sdk|react-native)/',
-  ],
-  testRegex: '/src/native/__tests__/.*(\\.|/)(test|spec)\\.[jt]sx?$',
-}
